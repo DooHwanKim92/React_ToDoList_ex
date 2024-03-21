70e5b579a97e0791721d16f4461e1bc910381c31
DooHwanKim92 <rmfpsvldj01@naver.com>
2024-03-21 17:38:46 +0900

    checked state change

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ function App() {
 
   let textId = useRef(4);
 
+  // todo sample data
   const [toDoList, setToDoList] = useState([
-    {id:1,text:"toDo-1"},
-    {id:2,text:"toDo-2"},
-    {id:3,text:"toDo-3"}
+    {id:1,text:"toDo-1",checked:false},
+    {id:2,text:"toDo-2",checked:false},
+    {id:3,text:"toDo-3",checked:false}
   ]);
 
+  // 추가
   const onInsert = (e) => {
     e.preventDefault();
-    setToDoList([...toDoList, {id:textId.current,text: e.target.text.value}]);
+    setToDoList([...toDoList, {id:textId.current,text: e.target.text.value,checked:false}]);
     textId.current++;
   }
 
+  // 삭제
   const onRemove = (id) => {
-      setToDoList(toDoList.filter((toDoList => toDoList.id !== id)))
-    }
+    setToDoList(toDoList.filter((toDoList => toDoList.id !== id)))
+  }
+
+  // check
+  const onChecked = (id) => {
+    setToDoList(toDoList.map((toDoList) => {
+      return toDoList.id === id ? {...toDoList, checked: !toDoList.checked} : toDoList
+    }))
+  }
 
   return (
     <div className="App">
@@ -29,8 +39,9 @@ function App() {
       </form>
       <ul>
         {toDoList.map(toDoList => 
-          <li>
-            <input type='checkbox'/> {toDoList.id}/{toDoList.text}
+          <li key={toDoList.id}>
+            <input type='checkbox' checked={toDoList.checked} onClick = {() => onChecked(toDoList.id)}/> 
+            {toDoList.id}/{toDoList.text}
             <button>modify</button>
             <button onClick={() => onRemove(toDoList.id)}>delete</button>
           </li>)
